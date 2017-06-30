71349cb15035bfe6b2f3d677db33d3ba07a40ca8
alacerda <alancordeiro@gmail.com>
2017-06-29 23:59:25 -0300

    rri-1.0: final version of the rri encoder and decoder

--- tools/rri-encoder.py
+++ tools/rri-encoder.py
@@ -7,7 +7,9 @@
 
 import random
 
-shellcode = "\x48\x5b\x90"
+shellcode =  \
+"\xeb\x14\x4d\x61\x6e\x64\x61\x20\x76\x65\x72\x20\x63\x61\x6d\x61\x72\x61\x64\x61\x21\x0a\x48\x31\xc0\x48\x83\xc0\x01\x48\x89\xc7\x48\x8d\x35\xdb\xff\xff\xff\x48\x31\xd2\x48\x83\xc2\x14\x0f\x05\x48\x31\xc0\x48\x83\xc0\x3c\x48\x31\xff\x48\x83\xc7\x01\x0f\x05"
+
 reverse = bytearray(shellcode[::-1])
 
 encode_1 = ""
@@ -20,7 +22,7 @@ for x in bytearray(shellcode):
 
     encode_2 += '0x'
     encode_2 += '%02x,' % x
-    encode_2 += 'x%02x,' % reverse[random.randint(1,len(shellcode) - 1)]
+    encode_2 += '0x%02x,' % reverse[random.randint(1,len(shellcode) - 1)]
 
 print "\n\tWe've generated two different encodes for you. Choose yours!"
 
